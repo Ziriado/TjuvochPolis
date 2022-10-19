ffaffa31f670136baa50c184e237305dafb7878f
Hypzter <cgranfeldt@hotmail.com>
2022-10-19 11:02:27 +0200

    Added Movement
    Added Movement

--- GitTjuvochPolis/GitTjuvochPolis/City.cs
+++ GitTjuvochPolis/GitTjuvochPolis/City.cs
@@ -34,5 +34,51 @@ namespace GitTjuvochPolis
             }
         }
 
+        public void TakeStep(int random)
+        {
+            int moveX = 0;
+            int moveY = 0;
+          
+            switch (random)//N S W E NW NE SW SE
+            {
+                case 1: //Norrut
+                    moveX = 0;
+                    moveY = -1;
+                    break;
+                case 2: //SÖder
+                    moveX = 0;
+                    moveY = 1;
+                    break;
+                case 3: //west
+                    moveX = -1;
+                    moveY = 0;
+                    break;
+                case 4: //east
+                    moveX = 1;
+                    moveY = 0;
+                    break;
+                case 5: //northwest
+                    moveX = -1;
+                    moveY = -1;
+                    break;
+                case 6: //northeast
+                    moveX = +1;
+                    moveY = -1;
+                    break;
+                case 7: //southwest
+                    moveX = -1;
+                    moveY = +1;
+                    break;
+                case 8: //southeast
+                    moveX = +1;
+                    moveY = +1;
+                    break;
+                case 9: // standstill
+                    moveX = 0;
+                    moveY = 0;
+                    break;                    
+            }
+        }
+
     }
 }

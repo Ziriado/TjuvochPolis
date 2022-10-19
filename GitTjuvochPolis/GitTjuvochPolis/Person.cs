096bb03cfa5cec93b6dd6dadb4b9d828cb019a7c
Hypzter <cgranfeldt@hotmail.com>
2022-10-19 10:22:32 +0200

    Added subclasses
    Added subclasses

--- GitTjuvochPolis/GitTjuvochPolis/Person.cs
+++ GitTjuvochPolis/GitTjuvochPolis/Person.cs
@@ -8,5 +8,39 @@ namespace GitTjuvochPolis
 {
     internal class Person
     {
+       public int SetX { get; set; }
+
+        public int SetY { get; set; }
+
+        //public int Direction { get; set; }
+
+        public virtual char Symbol { get; set; }
+
+        public Person(int setX, int setY)
+        {
+            this.SetX = setX;
+            SetY = setY;
+        }
+    }
+    class Citizen : Person
+    {
+        public Citizen(int SetX, int SetY, int Direction) : base(SetX, SetY)
+        {
+            Symbol = 'M';
+        }
+    }
+    class Thief : Person
+    {
+        public Thief(int SetX, int SetY, int Direction) : base(SetX, SetY)
+        {
+            Symbol = 'T';
+        }
+    }
+    class Police : Person
+    {
+        public Police(int SetX, int SetY, int Direction) : base(SetX, SetY)
+        {
+            Symbol = 'P';
+        }
     }
 }

bb795bc7d1df3e9cd70d9b17288bd88c8ec98c12
Hypzter <cgranfeldt@hotmail.com>
2022-10-19 10:41:56 +0200

    Updated Main
    Updated Main

--- GitTjuvochPolis/GitTjuvochPolis/Program.cs
+++ GitTjuvochPolis/GitTjuvochPolis/Program.cs
@@ -5,7 +5,21 @@
         static void Main(string[] args)
         {
             City city = new City();
-            city.DrawCity();
+            
+            Random rnd = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                city.AddPerson(new Citizen(rnd.Next(0, 100), rnd.Next(0, 25)));
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                city.AddPerson(new Thief(rnd.Next(0, 100), rnd.Next(0, 25)));
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                city.AddPerson(new Police(rnd.Next(0, 100), rnd.Next(0, 25)));
+            }
 
         }
     }

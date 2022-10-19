e8704cf94d9da612b40fcc53c9a1dde9c25fa687
maryyj <maria.holster@outlook.com>
2022-10-19 12:55:42 +0200

    Updated for loops

--- GitTjuvochPolis/GitTjuvochPolis/Program.cs
+++ GitTjuvochPolis/GitTjuvochPolis/Program.cs
@@ -8,28 +8,27 @@
             
             Random rnd = new Random();
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 1; i++)
             {
-                city.AddPerson(new Citizen(rnd.Next(0, 100), rnd.Next(0, 25)));
+                city.AddPerson(new Citizen(rnd.Next(0, 25 ), rnd.Next(0, 100)));
             }
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 1; i++)
             {
-                city.AddPerson(new Thief(rnd.Next(0, 100), rnd.Next(0, 25)));
+                city.AddPerson(new Thief(rnd.Next(0, 25), rnd.Next(0, 100)));
             }
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 1; i++)
             {
-                city.AddPerson(new Police(rnd.Next(0, 100), rnd.Next(0, 25)));
+                city.AddPerson(new Police(rnd.Next(0, 25), rnd.Next(0, 100)));
             }
             while (true)
             {
-                //foreach (Person person in city.people)
-                //{
-                //    person.TakeStep();
-                //}
+                foreach (Person person in city.people)
+                {
+                    person.TakeStep();
+                }
                 city.DrawCity();
                 Thread.Sleep(100);
             }
-
         }
     }
 }

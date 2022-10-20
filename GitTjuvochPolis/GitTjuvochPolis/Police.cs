08dc71d23385097182558873648f39ae617b38ef
maryyj <maria.holster@outlook.com>
2022-10-20 15:28:37 +0200

    SubClasses in Files

---
+++ GitTjuvochPolis/GitTjuvochPolis/Police.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTjuvochPolis
+{
+    internal class Police : Person
+    {
+        public Police(int SetX, int SetY) : base(SetX, SetY)
+        {
+            Symbol = 'P';
+        }
+    }
+}
